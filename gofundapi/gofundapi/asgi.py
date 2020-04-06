493cde71c487b39bd8d881d80aef777c3863a2fb
Kyle Harline <kylehar9@gmail.com>
2020-04-06 11:01:55 -0600

    Started a django project called gofundapi

---
+++ gofundapi/gofundapi/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for gofundapi project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gofundapi.settings')
+
+application = get_asgi_application()
