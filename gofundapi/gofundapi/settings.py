20a74480b8b78960789c5b16fcdc178a6a8a6dcc
Kyle Harline <kylehar9@gmail.com>
2020-04-06 11:32:50 -0600

    Got a basic testpage view working

--- gofundapi/gofundapi/settings.py
+++ gofundapi/gofundapi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = { 'DEFAULT_PERMISSION_CLASSES': [ 'rest_framework.permissions.AllowAny' ] }
+
+# GSC
